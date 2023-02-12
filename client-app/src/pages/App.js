9f81699afd99947318bc8e17b33711fe80484596
Matan Peretz <matanperetz@outlook.com>
2023-02-12 11:54:58 +0200

    Creating File page
    - Installing CodeMirror component to allow File content editing.
    - Creating FilePage component(UI only)

--- client-app/src/pages/App.js
+++ client-app/src/pages/App.js
@@ -2,17 +2,19 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Home from './Home';
-import RepositoryPage from './RepositoryPage';
 import repositories from '../test data/repositories';
+import FilePage from './FilePage';
 
 function App() {
+    const folderPath = 
+    repositories[0].name + "/" +
+    repositories[0].branches[0].currentCommit.rootFolder.name + "/";
+    console.log("FULLFILEPATH=", folderPath)
     return (
         <>
             <Header/>
             <Container sx={{margin: "10px"}}>
-            {/* <Home/> */}
-            <RepositoryPage repository={repositories[0]}/>
+            <FilePage file={repositories[0].branches[0].currentCommit.rootFolder.fileContent[0]} fileFolderPath={folderPath}/>
             </Container>
             <Footer/>
         </>
