baa2ae7a2e5d27bfb412a03c2040a107280ef5ca
Matan Peretz <matanperetz@outlook.com>
2023-02-21 16:01:25 +0200

    Refactoring Repository Page
    - Splitting the RepositoryPage.js to 2 files
    - Spliting to more sub components.

---
+++ client-app/src/components/RepositoryBranchView.js
@@ -0,0 +1,63 @@
+import { Button, MenuItem, Select, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import FilesTree from "../components/FilesTree";
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import AltRouteIcon from '@mui/icons-material/AltRoute';
+
+function BranchNavBar({repoBranchName,branchesNames, setBranch}){
+    function handlesSelectBranch(event){
+        const selectedBranchName = event.target.value;
+        setBranch(selectedBranchName);
+    }
+
+    return (
+        <Container sx={{display: "flex", }}>
+            <Container sx={{ justifyContent: "center", flexDirection: "row", maxWidth: "initial" }}>
+
+                <Typography variant="p" fontSize={24} noWrap>
+                    active branch:
+                </Typography>   
+                <Select
+                    value={repoBranchName}
+                    onChange={handlesSelectBranch}
+                    size="small"
+                >
+                    {branchesNames.map((branchName, index)=>{
+                        return (
+                            <MenuItem key={index} value={branchName}>
+                                {branchName}
+                            </MenuItem>
+                        )
+                    })}                  
+                </Select>
+            </Container>
+            <Container sx={{alignSelf:"center", justifyContent:"right", flexDirection: "row-reverse"}}>
+                <Button variant="outlined" 
+                        
+                        onClick={()=>{/* TODO: Create new branch form */}}
+                        sx={{mr: 3}}
+                        >
+                        <AltRouteIcon/>
+                        <Typography variant="p" fontSize={"small"} >
+                            New branch
+                        </Typography>
+                </Button>
+                <Button>
+                    <UploadFileIcon/>
+                    Add new File
+                </Button>
+
+            </Container>
+        </Container>
+    );
+}
+
+export default function RepositoryBranchView({setBranch, currCommit, currBranch, branchesList}){
+
+    return (
+        <Container >
+            <BranchNavBar repoBranchName={currBranch} branchesNames={ branchesList} setBranch={setBranch}/>
+            <FilesTree currCommit={currCommit}/>
+        </Container>
+    )
+}
--- client-app/src/pages/RepositoryPage.js
+++ client-app/src/pages/RepositoryPage.js
@@ -1,61 +1,12 @@
-import { Button, MenuItem, Select, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
-import FilesTree from "../components/FilesTree";
-import UploadFileIcon from '@mui/icons-material/UploadFile';
-import AltRouteIcon from '@mui/icons-material/AltRoute';
-import ObjectMapContext from "../context/ObjectMapContext";
+import RepositoryBranchView from "../components/RepositoryBranchView";
+import ObjectsMapContext from "../context/ObjectMapContext";
 
-function BranchNavBar({repoBranchName,branchesNames, setBranch}){
-    function handlesSelectBranch(event){
-        const selectedBranchName = event.target.value;
-        setBranch(selectedBranchName);
-    }
 
-    return (
-        <Container sx={{display: "flex"}}>
-            <Container sx={{ justifyContent: "start", flexDirection: "row", maxWidth: "initial" }}>
 
-                <Typography variant="p" fontSize={24} noWrap>
-                    active branch:
-                </Typography>   
-                <Select
-                    value={repoBranchName}
-                    onChange={handlesSelectBranch}
-                    size="small"
-                >
-                    {branchesNames.map((branchName, index)=>{
-                        return (
-                            <MenuItem key={index} value={branchName}>
-                                {branchName}
-                            </MenuItem>
-                        )
-                    })}                  
-                </Select>
-            </Container>
-            <Container sx={{alignSelf:"center", justifyContent:"right", flexDirection: "row-reverse"}}>
-                <Button variant="outlined" 
-                        
-                        onClick={()=>{/* TODO: Create new branch form */}}
-                        sx={{mr: 3}}
-                        >
-                        <AltRouteIcon/>
-                        <Typography variant="p" fontSize={"small"} >
-                            New branch
-                        </Typography>
-                </Button>
-                <Button>
-                    <UploadFileIcon/>
-                    Add new File
-                </Button>
-
-            </Container>
-        </Container>
-    );
-}
-
-function RepositoryView({repository}){
-    // console.log(repository);
+function RepositoryPage({repository}) {
     const [currBranch, setCurrBranch] = useState(repository.currBranch);
     const currBranchCommit = repository.branches.get(repository.currBranch)
     const [currCommit, setCurrCommit] = useState(repository.objectsMap.get(currBranchCommit));    
@@ -64,25 +15,21 @@ function RepositoryView({repository}){
         setCurrCommit(repository.objectsMap.get(currBranchCommit));
         setCurrBranch(newBranch);
     }
-
-    return (
-        <Container >
-            <BranchNavBar repoBranchName={currBranch} branchesNames={[...repository.branches.keys()]} setBranch={setBranchCommit}/>
-            <ObjectMapContext.Provider value={repository.objectsMap}>
-                <FilesTree currCommit={currCommit}/>
-            </ObjectMapContext.Provider>
-        </Container>
-    )
-}
-
-
-function RepositoryPage({repository}) {
     return (
         <Container className={"repositoryView"}>
             <Typography variant="h3" gutterBottom>
                 {`${repository.creator}/${repository.name}`} 
             </Typography>
-            <RepositoryView repository={repository}/>
+            <ObjectsMapContext.Provider value={repository.objectsMap}>
+                <RepositoryBranchView 
+                    setBranch={setBranchCommit} 
+                    currCommit={currCommit} 
+                    currBranch={currBranch} 
+                    branchesList={[...repository.branches.keys()]}
+
+                    />
+
+            </ObjectsMapContext.Provider>
         </Container>
     );
 }
